145001e237ce8d4a6c1032080e8cd59fb631e1dc
ashutoshaay26 <ashutoshkakadiya11@gmail.com>
2019-10-23 20:09:57 +0530

    Q3 without coordinate

--- Assignment_2/Question_3.py
+++ Assignment_2/Question_3.py
@@ -28,6 +28,15 @@ def read_data(filename):
 	# data : nxd, label : nx1
 	return (data,label)
 
+# Split the data into train and test data.
+def split_data(x,y,ratio=0.2):
+	s = int(x.shape[0]*0.2)
+	train_data =  x[:s,:]
+	train_label = y[:s]
+	val_data =  x[s+1:,:]
+	val_label = y[s+1:]
+
+	return train_data,train_label,val_data,val_label
 
 # Closed Form Solution w = (x.T*x)^-1 * x.T * Y 
 def closed_form_gd(x,y):
@@ -35,9 +44,9 @@ def closed_form_gd(x,y):
 
 
 # Gradient Descent for ridge regression
-def gradient_descent(x,y,w,lamb,step_size,epoch=100):
+def gradient_descent(x,y,weights,lamb,step_size,epoch=100):
 
-	weights_t =[weights]
+	#weights_t =[weights]
 	#cost=[]
 	for i in range(epoch):
 		pred = np.dot(data,weights)
@@ -47,63 +56,90 @@ def gradient_descent(x,y,w,lamb,step_size,epoch=100):
 
 		# Update the weights (in matrix form)
 		weights = weights - step_size * ((1/data.shape[0]) * np.dot(np.transpose(data),error)+ lamb*weights )
-		weights_t.append(weights)
+		#weights_t.append(weights)
 		#cost.append(error)
-	return weights_t
+	return weights
 
 # Return the list of error for different set of lambdas.
-def cross_validation_lamb(x,y,w,lamb,step_size,epoch=100):
-
+def cross_validation_lamb(train_data,train_label,val_data,val_label,w,list_lambda,step_size,epoch=100):
+	error_list=[]
+	current_cost=1000000
+	best_lambda=0.1
+	best_weights=w
+	for i in range(len(list_lambda)):
+		w = gradient_descent(train_data,train_label,w,list_lambda[i],step_size,epoch)
+		temp_error = compute_error_r(val_data,val_label,w,list_lambda[i])
+		error_list.append(temp_error)
+		if temp_error < current_cost:
+			best_lambda=list_lambda[i]
+			best_weights=w
 	return error_list,best_lambda,best_weights
 
-# Computer error using loss function
-def compute_error(x,y,w,lamb):
-	pass
+# Computer error using loss function for closed form solution w_ml
+def compute_error_ml(x,y,w):
+	cost = (1/(2*x.shape[0])) * (np.sum( (np.dot(x,w) - y)**2 )  )
+	return cost
+
+# Computer error using loss function for Ridge rigression solution w_r
+def compute_error_r(x,y,w,lamb):
+	cost = (1/(2*x.shape[0])) * (np.sum( (np.dot(x,w) - y)**2 ) + lamb * np.dot(np.transpose(w),w) )
+	return cost
 
 
-# Predict on Test Data
-def predict():
-	pass
 
+# Plot Test Error.
+def plot_test_error(test_x,test_y,w_r,w_ml,best_lambda):
+	error_r = compute_error_r(test_x,test_y,w_ml,best_lambda)
+	error_ml = compute_error_ml(test_x,test_y,w_ml)
+	plt.plot(error_r,'r')
+	plt.label("Test Error W_r")
+	plt.plot(error_ml,'b')
+	plt.label("Test Error W_ml")
 
-# plot data
-def plot_data_two(w_t,w_ml):
-	cost = []
-	for i in w_t:
-		cost.append(np.linalg.norm(i-w_ml))
-
-	plt.style.use('seaborn-whitegrid')
-	ax = plt.plot(cost)
-	plt.title("Absolute difference ||w_t - w_ml||")
-	plt.xlabel("Timestemp")
-	plt.ylabel("Frobenious Norm")
-	plt.show()
-	#plt.savefig("")
-	#plt.close()
-
-def plot_cost(c1,c2):
-
-	#plt.style.use('seaborn-whitegrid')
-	plt.plot(c1,'r')
-	#plt.plot(c2,'g')
 	plt.title("Test Error")
-	plt.xlabel("Timestemp")
+	plt.xlabel("Timestem")
 	plt.ylabel("Prediction Cost")
 	plt.show()
 
 
+# Plot Cross Validation error for different lambdas.
+def plot_cross_val_lambda(error,list_lambda,step_size,epoch):
+	plt.style.use('seaborn-whitegrid')
+	
+	plt.plot(list_lambda,error)
+	plt.title("Cross Validation for different valus of lambdas "+"step size:"+str(step_size) +" Epoch:"+str(epoch))
+	plt.xlabel("Lambda value")
+	plt.ylabel("Validation Error")
+	plt.show()
+
 
 if __name__ == "__main__":
-	file_path = "Data/Dataset_train.csv"
-	data,label=read_data(file_path) # Read the data. Return numpy array
+	file_path1 = "Data/Dataset_train.csv"
+	data,label=read_data(file_path1) # Read the data. Return numpy array
+
+	file_path2 = "Data/Dataset_test.csv"
+	test_data,test_label=read_data(file_path2) # Read the data. Return numpy array
+
 	
+	# Parameter Initialization	
 	epoch=100
 	step_size=0.01
 	batch_size=100
+	split_ratio=0.2
+	list_lambda = [0.01,0.07,0.2,0.5,0.9,1,1.5,1.8,2]
+
 	weights = np.random.rand(100)
 	
-	w_t = gradient_descent(data,label,weights,step_size,epoch)
-	#w_t = stochastic_gradient_descent(data,label,weights,step_size,batch_size,epoch)
-	w_ml=closed_form_gd(data,label)
-	plot_data_two(w_t,w_ml)
-	#plot_cost(cost1,cost2)
+	train_data,train_label,val_data,val_label = split_data(data,label,split_ratio)
+
+	
+	error_list,best_lambda,w_r = cross_validation_lamb(train_data,train_label,val_data,val_label,weights,list_lambda,step_size,epoch)
+
+
+	plot_cross_val_lambda(error_list,list_lambda,step_size,epoch)
+
+	w_ml=closed_form_gd(train_data,train_label)
+
+
+	print("Test Error, w_ml : ",compute_error_ml(test_data,test_label,w_ml))
+	print("Test Error, w_r : ",compute_error_r(test_data,test_label,w_r,best_lambda))
